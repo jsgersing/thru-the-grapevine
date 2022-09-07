5e4a11aee731fbfaf2445bc2a8eb03edc7a54f96
jsgersing <jsgersing@gmail.com>
2022-09-06 18:30:31 -0700

    Create schema validation file

---
+++ schema_validation.py
@@ -0,0 +1,19 @@
+from generators import RandomGrapeBuyer, RandomGrapeSeller
+from schema import GrapeBuyer, GrapeSeller
+
+
+def validate_grape_buyer():
+    assert all(GrapeBuyer(**vars(RandomGrapeBuyer())) for _ in range(1000))
+
+
+def validate_grape_seller():
+    assert all(GrapeSeller(**vars(RandomGrapeSeller)) for _ in range(1000))
+
+
+def validate_schemas():
+    validate_grape_buyer()
+    validate_grape_seller()
+
+
+if __name__ == '__main__':
+    validate_schemas()
