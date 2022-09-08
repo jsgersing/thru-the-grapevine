c73b14e90254c0a119632126db365986d64d646f
jsgersing <jsgersing@gmail.com>
2022-09-07 17:11:49 -0700

    Refactor file structre

--- data_generators/data_options.py
+++ data_generators/data_options.py
@@ -1,4 +1,7 @@
 import random
+import string
+from itertools import chain
+from math import ceil, floor
 
 import names
 
@@ -25,8 +28,8 @@ grape_variety = (
 
 )
 
-grape_buying = random.sample(grape_variety, k=random.randint(1, 4))
-grape_selling = random.sample(grape_variety, k=random.randint(1, 4))
+grapes_seeking = random.sample(grape_variety, k=random.randint(1, 4))
+grapes_selling = random.sample(grape_variety, k=random.randint(1, 4))
 
 state = (
     "California", "Washington", "Oregon"
@@ -35,8 +38,19 @@ state = (
 state_buying = random.choice(state)
 state_selling = random.choice(state)
 
-tons_seeking = [random.randint(1, 10) * 5 for _ in grape_buying]
-tons_selling = [random.randint(1, 10) * 5 for _ in grape_selling]
+tons_seeking = [random.randint(1, 10) * 5 for _ in grapes_seeking]
+tons_selling = [random.randint(1, 10) * 5 for _ in grapes_selling]
+
+
+def generate_uuid(n_len: int) -> str:
+    n1 = ceil(n_len / 2)
+    n2 = floor(n_len / 2)
+    prefix = random.choices(string.ascii_letters, k=n1)
+    suffix = map(str, random.choices(range(0, 9), k=n2))
+    uuid_list = list(chain(prefix, suffix))
+    random.shuffle(uuid_list)
+    uuid = "".join(uuid_list)
+    return uuid
 
 
 if __name__ == "__main__":
@@ -44,13 +58,13 @@ if __name__ == "__main__":
         f"""Buyer: {winery_name}
         Contact: {first_name_buyer} {last_name_buyer}
         Email: {first_name_buyer.lower()}.{last_name_buyer.lower()}@{random_email_buyer}
-        Grape Variety: {grape_buying}, 
+        Grape Variety: {grapes_seeking}, 
         Volume: {tons_seeking} tons
         State: {state_buying}""")
     print(
         f"""Seller: {vineyard_name}
         Contact: {first_name_seller} {last_name_seller}
         Email: {first_name_seller.lower()}.{last_name_seller.lower()}@{random_email_seller}
-        Grape Variety: {grape_selling}, 
+        Grape Variety: {grapes_selling}, 
         Volume: {tons_selling} tons
         State: {state_selling}""")
